8ca7ff47235c5df2b4f5a019240aaa8a3399da04
James McDougall <jameswilliammcdougall@gmail.com>
2021-01-05 22:07:30 -0600

    Created Wine Category ViewController

---
+++ Wine Flight/Wine Flight/Views/CustomUI/CustomUI.swift
@@ -0,0 +1,20 @@
+//
+//  CustomUI.swift
+//  Wine Flight
+//
+//  Created by James McDougall on 1/5/21.
+//
+
+import UIKit
+
+class CustomUI {
+    
+    func roundedView(view: UIView) {
+        view.layer.cornerRadius = 25
+    }
+    
+    func roundedImage(image: UIImageView) {
+        image.layer.cornerRadius = 25
+    }
+    
+}
