e1efaea0b7b6786c0e7afd086109d93b2b288c50
Bohdan Tkachenko <tech.bohdan@gmail.com>
2021-01-08 19:45:24 -0600

    Added ID button, DetailView etc

---
+++ Wine Flight/Wine Flight/Models/Product.swift
@@ -0,0 +1,30 @@
+//
+//  Product.swift
+//  Wine Flight
+//
+//  Created by Bohdan Tkachenko on 1/8/21.
+//
+
+import Foundation
+
+import UIKit
+
+class Product {
+    
+//    let wineSelection: [WineSelection]
+    let image: UIImage
+    let amount: Int
+     
+    
+    internal init( wineSelection: WineSelection, image: UIImage, amount: Int) {
+//        self.wineSelection = WineSelectionController.wineSelection
+        self.image = image
+        self.amount = amount
+    }
+    
+    
+    
+    
+}
+
+
